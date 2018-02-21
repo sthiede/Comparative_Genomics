9ea0ffe2ed979cefec8b6936692e641b8a370c60
Ali Pirani <alipirani88@gmail.com>
2018-02-21 15:06:00 -0500

    read the docs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Micro 612 genomics workshop\u2019s documentation!"],titleterms:{document:0,genom:0,indic:0,micro:0,tabl:0,welcom:0,workshop:0}})
